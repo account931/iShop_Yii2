be77415ae490717437c58569b25d723cef30d57b
account931 <account931@ukr.net>
2018-05-07 18:05:11 +0300

    Add files via upload

--- models/Products.php
+++ models/Products.php
@@ -29,7 +29,7 @@ class Products extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['pr_name', 'pr_description', 'pr_price', 'pr_image'], 'required'],
+            [['pr_name', 'pr_description', 'pr_price', ], 'required'],
             [['pr_price'], 'number'],
             [['pr_name'], 'string', 'max' => 55],
             [['pr_description'], 'string', 'max' => 222],
@@ -50,4 +50,33 @@ class Products extends \yii\db\ActiveRecord
             'pr_image' => 'Pr Image',
         ];
     }
+	
+	
+	
+	
+	
+	//-------
+	public static function displayProducts()
+	{
+		$colorClass = array("list-group-item-success", "list-group-item-info", "list-group-item-warning", "list-group-item-danger"); // bootstrap Classes
+		$colorCount = 0;
+		
+		$query=Products::find()->orderBy ('pr_id DESC') /*->andFilterWhere(['like', 'sData_text', Yii::$app->getRequest()->getQueryParam('q')])*/  /*->where(['sData_text'=>Yii::$app->getRequest()->getQueryParam('q') ])*/ ->all();
+
+	    foreach ($query as $dataFromCtrl) {
+			//echo "<br>" . $dataFromCtrl->p r_name;
+			echo '<a href="#" class="list-group-item myBtn ' . $colorClass[$colorCount]  . ' " id='  .  $dataFromCtrl->pr_name . '-' .$dataFromCtrl->pr_price .'>';   
+			echo '<div class="row"><div class="col-sm-5"> <img class="prod-img" src="images/product_icon.png" alt=""/> First item : <b>' . $dataFromCtrl->pr_name . '</b> <br>';
+			echo '<img class="packaging" src="images/packaging.png"/>   </div>';
+			echo '<div class="col-sm-5 textX">Product text description <br>Price details : ' .$dataFromCtrl->pr_price. ' UAH</div></div></a>';
+			
+			if ($colorCount < 3) {
+				$colorCount++;
+			} else {
+				$colorCount = 0;
+			}
+		}
+	
+	}
+	//-----------
 }

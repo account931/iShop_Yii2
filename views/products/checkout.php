06493eb956b3545c7a7b7f97916b194387817f2c
account931 <account931@ukr.net>
2018-05-15 14:06:58 +0300

    Add files via upload
    Added Loader spinner on ajax + send ajax with Orders object, Unique order number, User address, mobile, gets the Php Json data

--- views/products/checkout.php
+++ views/products/checkout.php
@@ -10,6 +10,8 @@ use app\assets\CheckOutAssetOnly; // using my custom asset to use modal.js/mycor
 CheckOutAssetOnly::register($this); // register my custom asset to use modal.js/mycore.js Only in this View (1st name-> is the name of Class)
 
 
+//Uses Spinner Loader on while ajax {1.<img src="images/load-spinner.gif" id="loading-indicator". 2.#loading-indicator css + thickbox-open  {-webkit-filter: blur(2px); 3.$(document).ajaxSend(function(event, request, settings) { $('body').addClass('thickbox-open'); $('#loading-indicator').show(400);}
+
 /* @var $this yii\web\View */
 /* @var $searchModel app\models\Products_Search */
 /* @var $dataProvider yii\data\ActiveDataProvider */
@@ -21,7 +23,20 @@ $this->params['breadcrumbs'][] = $this->title;
 <style>
 .bordX {
     border: 1px solid #000000;
+}
+/* Ajax load image*/
+#loading-indicator {
+  position: absolute;
+  left: 40%;
+  top: 40%;
+}
 
+.thickbox-open  {
+  -webkit-filter: blur(2px);
+  -moz-filter: blur(2px);
+  -ms-filter: blur(2px);
+  -o-filter: blur(2px);
+  filter: blur(2px); 
 }
 </style>
 
@@ -90,9 +105,9 @@ $this->params['breadcrumbs'][] = $this->title;
                         'options' => ['id' => 'myForm', 'enableAjaxValidation' => 'true',]
                         ]); ?>
 
-            <?= $form->field($searchModel, 'username')->textInput(['maxlength' => true, 'value'=>$d]) ?>
-			<?= $form->field($searchModel, 'mobile')->textInput(['maxlength' => true]) ?>
-			<?= $form->field($searchModel, 'address')->textarea(['rows' => 3]) ?> 
+            <?= $form->field($searchModel, 'username')->textInput(['maxlength' => true, 'value'=>$d, 'id'=>'name_id']) ?>
+			<?= $form->field($searchModel, 'mobile')->textInput(['maxlength' => true, 'id'=>'mobile_id']) ?>
+			<?= $form->field($searchModel, 'address')->textarea(['rows' => 3, 'id'=>'address_id']) ?> 
 
             <div class="form-group">
                 <?= Html::submitButton('Send', ['class' => 'btn btn-success', 'id'=>'submit_id']) ?>
@@ -141,7 +156,7 @@ $this->params['breadcrumbs'][] = $this->title;
 		
 		
 </div> <!-- <div class="checkout-index">-->
-
+<img src="images/load-spinner.gif" id="loading-indicator" style="display:none" /> <!--- Animation while ajax Load Spinner-->
 
 
 
@@ -153,13 +168,30 @@ $this->params['breadcrumbs'][] = $this->title;
 $URL =Yii::$app->request->baseUrl. "/index.php?r=products/getajaxorderdata";   //WORKING,  gets the url to send ajax, defining it in  $ajax causes routing to 404, Not found, as the url address does not render correctly
 //url: 'http://localhost/iShop_yii/yii-basic-app-2.0.15/basic/web/index.php?r=products/getajaxorderdata',  // the correct address sample for ajax
 
-$Controller = Yii::$app->controller->id;
+$Controller = Yii::$app->controller->id; // to pass in ajax
 
 	//My working JS Register
 	//Checks in JS if the Validation runs fine 
 	$this->registerJs( <<< EOT_JS_CODE
+	// JS code here   //afterValidate
+	
+	// function to generate unique order number--------------------
+	function generateUUID() { // Public Domain/MIT
+        var d = new Date().getTime();
+        if (typeof performance !== 'undefined' && typeof performance.now === 'function'){
+            d += performance.now(); //use high-precision timer if available
+        }
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+            var r = (d + Math.random() * 16) % 16 | 0;
+            d = Math.floor(d / 16);
+            return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+       });
+      }
+	  // END  function to generate unique order number
+  //-----------------------------------------------------------------
+  
 
-  // JS code here   //afterValidate
+ 
    $("#myForm").on("beforeSubmit", function (event, messages) {
        // Now you can work with messages by accessing messages variable
        //var attributes = $(this).data().attributes; // to get the list of attributes that has been passed in attributes property
@@ -174,13 +206,15 @@ $Controller = Yii::$app->controller->id;
 		   
 	   } else { 
 	   
-	       alert("Validate OK");  //alert(<?php echo Yii::$app->request->baseUrl?> +"/products/getajaxorderdata" );
+	        alert("Validate OK");  //alert(<?php echo Yii::$app->request->baseUrl?> +"/products/getajaxorderdata" );
 		    // runs ajax here
-			//var data = $(this).serialize();
-			var data = "This";
+			//var userInput = $(this).serialize();  //user form input-FAILS
+			//alert("form " + userInput);
+			
+			// Start AJAX
             $.ajax({
-				    //url      : '<?php echo Yii::app()->createUrl("products/getajaxorderdata");?>',
-				  url: '$URL',  //WORKING
+			    //url      : '<?php echo Yii::app()->createUrl("products/getajaxorderdata");?>',
+		        url: '$URL',  //WORKING
 				//url: form.attr('getajaxorderdata'),
                 
                 type: 'post',
@@ -191,27 +225,66 @@ $Controller = Yii::$app->controller->id;
 				data: {
                     controller : '$Controller ',
 				    //_csrf : '<?= Yii::$app->request->csrfToken; ?>',
-				    searchname : 'Dima',
+				    searchname : 'Dima',  //just fot test, may drop later
+					userName : $('#name_id').val(), //passing the UserName info input
+					userCell : $('#mobile_id').val(), //passing cell mobile
+					userAddress : $('#address_id').val(), //passing address info input
+					uniqueOrderNumber : generateUUID() , // unique Order number
+					orderObject : JSON.stringify( productsObject ), // pass an Object with all order to php
                 },
+				
+				// if send was successful
                 success: function(res){
                     console.log(res);
-				    alert (res);
-				    alert (res.name); 
-					alert (res.result_status);
-				    textMine = res.result_status + "<br> Current Controller: " + res.controller; // formate the anwer to html
-					//thml with animation
+				    //alert (res);
+				    //alert (res.name); 
+					//alert (res.result_status);
+					
+					//Format the anwer based on Order Object
+					var formatted_allOrders = '<br>You have ordered:'; //will contain the whole orders answer
+					var priceTotal = 0; // total price of all oreders
+					
+					var objMine = JSON.parse(res.allOrders); // covert JSON part with orders to JS Object
+					//alert(JSON.stringify(objMine, null, 4));
+					
+					for (var key in objMine) {
+						 priceTotal = priceTotal + objMine[key]['price'] * objMine[key]['quantity'] ; // total price of all oreders
+						 formatted_allOrders = formatted_allOrders + "<br>" + key + ": " + objMine[key]['price'] + " * " + objMine[key]['quantity'] + " items = " + substringSum (objMine[key]['price'] * objMine[key]['quantity']) + " UAH";
+					 }
+                     formatted_allOrders = formatted_allOrders + "<br>Total: " + priceTotal + " UAH";	//adding total price of all oreders	 
+					
+				    textMine = res.result_status + "<br> Current Controller: " + res.controller + "<br>" +res.userformData + "<br>" +res.userCell + "<br>" +
+  					    res.userAddr + "<br>Order ID: " + res.orderNumber + "<br>OrderList:<br> " + formatted_allOrders; // formate the answer to html in view
+						
+					//html with animation
 					$('.checkout-index').stop().fadeOut("slow",function(){  $('.checkout-index').html(textMine) }).fadeIn(2000);
                 },
                 error: function(){
                      alert('Error from View!');
                 }
             });
-			// END runs ajax here
+			// END runs AJAX here
 		    return false; //prevent reloading/submitting
 		  
-	   } 
+	   } // end else
   });
+  
+  //Image to show while ajax load - Load Spinner--------
+  $(document).ajaxSend(function(event, request, settings) {
+      $('body').addClass('thickbox-open'); // adds blur class to body while loading
+	  //$('#loading-indicator').parents().removeClass("thickbox-open");
+      $('#loading-indicator').show(400);
+      
+  });
+
+  $(document).ajaxComplete(function(event, request, settings) {
+      $('body').removeClass('thickbox-open');  // removes blur class to body while loading
+      $('#loading-indicator').hide(1200);
+  });
+  //END Image to show while ajax load Load Spinner-------
+  
   // END JS code here		
+  
 
 EOT_JS_CODE
 );
